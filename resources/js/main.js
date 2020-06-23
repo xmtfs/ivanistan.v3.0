175f278ebeb6a38a69287ff3d0faa0accfae1230
xmtfs <nesalomiviistrazivac@gmail.com>
2020-06-23 19:56:11 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.   On branch master   Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   constitution.html
    	new file:   index.html
    	new file:   resources/documents/Constitution_of_Ivanistan_23_9_2018.docx
    	new file:   resources/images/favicon.png
    	new file:   resources/images/flag.jpg
    	new file:   resources/js/main.js
    	new file:   resources/navbar/navbar.html
    	new file:   template.html

---
+++ resources/js/main.js
@@ -0,0 +1,30 @@
+     $.get("resources/navbar/navbar.html", function(data){
+          $("nav").replaceWith(data);
+      });
+      
+      function addLeadingZero(n) {
+        if (n < 10) {
+            n = "0" + n;
+        }
+        return n;
+    }
+    
+    function windTheClock()
+    {
+        var d = new Date();
+        var timeZoneOffset = -d.getTimezoneOffset()/60;
+        d.setHours(d.getUTCHours() + timeZoneOffset); 
+        var h = d.getHours();
+        var m = d.getMinutes();
+        var s = d.getSeconds();
+        h = addLeadingZero(h);
+        m = addLeadingZero(m);
+        s = addLeadingZero(s);
+        document.all["time"].innerHTML = h + ":" + m + ":" + s;
+        setTimeout(function(){ windTheClock(timeZoneOffset) }, 1000);
+    }
+    
+    window.onload = function() {
+        windTheClock();
+    }
+    
